82225b20579bd9a48928f5657f836304bfa31207
echlau <ericlau96@hotmail.com>
2016-12-11 14:48:46 -0800

    setup
    Signed-off-by: echlau <ericlau96@hotmail.com>

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -131,4 +131,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Stock', 'Recipe', 'Supplies');
+$autoload['model'] = array('Stock', 'Ingredients');
